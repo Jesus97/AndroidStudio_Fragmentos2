5d9a4336742cf6e04852b5b86a0e0cc1b330ec45
Jesus97 <jesusesma97@gmail.com>
2017-10-06 08:18:14 +0200

    commit 3

--- app/src/main/java/com/iesvirgendelcarmen/dam/fragment02/Fragment02.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/fragment02/Fragment02.java
@@ -1,22 +1,34 @@
 package com.iesvirgendelcarmen.dam.fragment02;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Surface;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
 
 public class Fragment02 extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment02);
 
+        final Fragmento1 fragmento1 = new Fragmento1();
+        final Fragmento2 fragmento2 = new Fragmento2();
+
         FragmentManager FM = getSupportFragmentManager();
         FragmentTransaction FT = FM.beginTransaction();
-        Fragmento1 fragment = new Fragmento1();
-
-        FT.add(R.id.contenedor, fragment);
-        FT.commit();
+        WindowManager WinMa = getWindowManager();
+        Display Disp = WinMa.getDefaultDisplay();
+        if (Disp.getRotation() == Surface.ROTATION_90){
+            FT.replace(R.id.contenedor, fragmento1).commit();
+        }else{
+            FT.replace(R.id.contenedor, fragmento2).commit();
+        }
     }
 }
